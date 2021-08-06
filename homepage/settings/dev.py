ef6f2285e4f8676bf05b4cb71ef87ebb84e515b6
murat-polat <muratsdepo@gmail.com>
2021-08-07 00:26:58 +0200

    first commit

---
+++ homepage/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'q74@b@fb0$y1dv*%q3_4&jm&l_vyvmu)bx^$4wxm%#k!c(0uf8'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
